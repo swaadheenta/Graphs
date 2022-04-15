a5f264936b09b8683763de6305007306d14a6f8f
swaadheenta <gayatriagnani7@gmail.com>
2022-04-15 11:06:27 +0530

    first commit

---
+++ does_path_exists.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool ispossible=false;
+
+/*
+0 -1
+1- 2
+2-3
+0-3
+3-4
+4-5
+5-6
+4-6
+*/
+void doespathexits(int s,int d,map<int,vector<int>>mp,map<int,int>visited)
+{
+     if(s==d)
+     {
+         cout<<"s= "<<s<<"d= "<<d<<"\n";
+         ispossible=true;
+         cout<<ispossible;
+         return;
+     }
+     vector<int>v=mp[s];
+     cout<<"s= "<<s;
+     visited[s]=true;
+     for(int i=0;i<v.size();i++)
+     {
+          cout<<"v[i] = "<<v[i]<<" ";
+         if(visited[v[i]]==false)
+         {
+            cout<<"vis[i] "<<v[i]<<"\n";
+             doespathexits(v[i],d,mp,visited);
+              // visited[v[i]]=true;
+         }
+     }
+     return;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int e;
+    cin>>e;
+    map<int,int>visited;
+    for(int i=0;i<n;i++)
+    {
+        visited[i]=false;
+    }
+    map<int,vector<int>>mp;
+    while(e--)
+    {
+        int s,d;
+        cin>>s>>d;
+       // 0 -> 1 3 
+       // 1 -> 0 2
+       // 2 -> 1 3
+       // 3 -> 2 0 4
+       // 4 -> 3 5 6
+       // 5 -> 4 6
+       // 6 -> 5 4 
+
+        mp[s].push_back(d);
+        mp[d].push_back(s);
+
+    }
+    
+    int src,dest;
+    cin>>src>>dest;
+
+    doespathexits(src,dest,mp,visited);
+    if(ispossible)
+    cout<<"true";
+    else
+    cout<<"false";
+    
+
+}
